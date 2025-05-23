bfe23c18dfaac010b67b5ec454a3c0226ec5ae43
_Tradam <github@tradam.dev>
2025-05-23 20:32:45 +0900

    change cube to purple

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshLambertMaterial( { color: 0x9370db } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
